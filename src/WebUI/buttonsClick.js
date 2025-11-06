5ccbc0d9178d163fe4e3786e72e62d9bc522a21f
UnderKo <UnderKo@DESKTOP-P1BPFCJ>
2025-11-06 19:17:06 +0300

    Enhance web UI with improved button styles and functionality

--- src/WebUI/buttonsClick.js
+++ src/WebUI/buttonsClick.js
@@ -1,11 +1,23 @@
-function btnClick( va , hesh ) {
-    fetch('/btnClick?hesh=' + hesh + '&id='+ va )
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => console.log('Toggle response:', data))
-        .catch(error => console.error('Fetch error:', error));
+let _count = 0;
+
+function settext(number) {
+   document.getElementById("hidtext").innerText = number;
+}
+
+function btnClick(va, hesh) {
+   fetch("/btnClick?hesh=" + hesh + "&id=" + va)
+      .then((response) => {
+         if (!response.ok) {
+            throw new Error("Network response was not ok");
+         }
+         _count++;
+
+         settext(_count);
+
+         return response.json();
+      })
+      .then((data) => {
+         console.log("Toggle response:", data);
+      })
+      .catch((error) => console.error("Fetch error:", error));
 }
